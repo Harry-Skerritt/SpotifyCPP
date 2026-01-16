5e361aa3fcaecf36e3e83edee4e1e29e79a4a77b
Harry Skerritt <harry2.skerritt@live.uwe.ac.uk>
2026-01-16 21:38:26 +0000

    Added an extensions header - not published

---
+++ include/spotify/extensions.hpp
@@ -0,0 +1,12 @@
+//
+// Created by harryskerritt on 16/01/2026.
+//
+
+#ifndef SPOTIFYCPP_EXTENSIONS_HPP
+#define SPOTIFYCPP_EXTENSIONS_HPP
+
+#pragma once
+
+#include "spotify/extensions/VisualAPI.hpp"
+
+#endif //SPOTIFYCPP_EXTENSIONS_HPP
